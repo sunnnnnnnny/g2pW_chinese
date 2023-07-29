2a6b3379fb942687301c7bbd1d4531afa71283c4
zhangsan <zhangsan@zhangsandeMacBook-Pro.local>
2023-07-29 13:45:19 +0800

    simplify api.py support chinese

--- g2pw/api.py
+++ g2pw/api.py
@@ -5,137 +5,95 @@ import zipfile
 from io import BytesIO
 import shutil
 
+import torch
 from torch.utils.data import DataLoader
 from transformers import BertTokenizer
-from tqdm import tqdm
-import onnxruntime
-import numpy as np
-
-from g2pw.dataset import TextDataset, get_phoneme_labels, get_char_phoneme_labels
+from pypinyin import pinyin
+from pypinyin import Style
+from g2pw.module import G2PW
+from g2pw.dataset import prepare_data, TextDataset, get_phoneme_labels, get_char_phoneme_labels
 from g2pw.utils import load_config
 
-MODEL_URL = 'https://storage.googleapis.com/esun-ai/g2pW/G2PWModel-v2-onnx.zip'
 
+def predict(model, dataloader, device, labels):
+    model.eval()
 
-def predict(onnx_session, dataloader, labels, turnoff_tqdm=False):
     all_preds = []
     all_confidences = []
+    with torch.no_grad():
+        generator = dataloader
+        for data in generator:
+            input_ids, token_type_ids, attention_mask, phoneme_mask, char_ids, position_ids = \
+                [data[name].to(device) for name in
+                 ('input_ids', 'token_type_ids', 'attention_mask', 'phoneme_mask', 'char_ids', 'position_ids')]
+            print(input_ids.shape)
+            probs = model(
+                input_ids=input_ids,
+                token_type_ids=token_type_ids,
+                attention_mask=attention_mask,
+                phoneme_mask=phoneme_mask,
+                char_ids=char_ids,
+                position_ids=position_ids
+            )
 
-    generator = dataloader if turnoff_tqdm else tqdm(dataloader, desc='predict')
-    for data in generator:
-        input_ids, token_type_ids, attention_mask, phoneme_mask, char_ids, position_ids = \
-            [data[name] for name in ('input_ids', 'token_type_ids', 'attention_mask', 'phoneme_mask', 'char_ids', 'position_ids')]
-
-        probs = onnx_session.run(
-            [],
-            {
-                'input_ids': input_ids.numpy(),
-                'token_type_ids': token_type_ids.numpy(),
-                'attention_mask': attention_mask.numpy(),
-                'phoneme_mask': phoneme_mask.numpy(),
-                'char_ids': char_ids.numpy(),
-                'position_ids': position_ids.numpy()
-            }
-        )[0]
-
-        preds = np.argmax(probs, axis=-1)
-        max_probs = probs[np.arange(probs.shape[0]), preds]
-
-        all_preds += [labels[pred] for pred in preds.tolist()]
-        all_confidences += max_probs.tolist()
+            max_probs, preds = map(lambda x: x.cpu().tolist(), probs.max(dim=-1))
+            all_preds += [labels[pred] for pred in preds]
+            all_confidences += max_probs
 
     return all_preds, all_confidences
 
 
-def download_model(model_dir):
-    root = os.path.dirname(os.path.abspath(model_dir))
-
-    r = requests.get(MODEL_URL, allow_redirects=True)
-    zip_file = zipfile.ZipFile(BytesIO(r.content))
-    zip_file.extractall(root)
-    source_dir = os.path.join(root, zip_file.namelist()[0].split('/')[0])
-    shutil.move(source_dir, model_dir)
-
-
 class G2PWConverter:
-    def __init__(self, model_dir='G2PWModel/', style='bopomofo', model_source=None, num_workers=None, batch_size=None,
+    def __init__(self, model_dir='/home/duser/tts/fastSpeech2-master/g2pw_models',  model_source=None,
+                 use_cuda=True, num_workers=None, batch_size=None,
                  turnoff_tqdm=True, enable_non_tradional_chinese=False):
-        if not os.path.exists(os.path.join(model_dir, 'version')):
-            download_model(model_dir)
+        # assert  os.path.exists(os.path.join(model_dir, 'best_accuracy.pth'))
+        assert os.path.exists(os.path.join(model_dir, 'best_accuracy.pth'))
 
-        sess_options = onnxruntime.SessionOptions()
-        sess_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_ALL
-        sess_options.execution_mode = onnxruntime.ExecutionMode.ORT_SEQUENTIAL
-        sess_options.intra_op_num_threads = 2
-        self.session_g2pw =  onnxruntime.InferenceSession(os.path.join(model_dir, 'g2pw.onnx'), sess_options=sess_options)
-        
         self.config = load_config(os.path.join(model_dir, 'config.py'), use_default=True)
-
         self.num_workers = num_workers if num_workers else self.config.num_workers
         self.batch_size = batch_size if batch_size else self.config.batch_size
         self.model_source = model_source if model_source else self.config.model_source
         self.turnoff_tqdm = turnoff_tqdm
+        self.enable_opencc = enable_non_tradional_chinese
 
-        self.tokenizer = BertTokenizer.from_pretrained(self.model_source)
+        self.device = torch.device('cuda' if use_cuda else 'cpu')
+
+        self.tokenizer = BertTokenizer.from_pretrained(self.config.model_source)
 
         polyphonic_chars_path = os.path.join(model_dir, 'POLYPHONIC_CHARS.txt')
         monophonic_chars_path = os.path.join(model_dir, 'MONOPHONIC_CHARS.txt')
         self.polyphonic_chars = [line.split('\t') for line in open(polyphonic_chars_path).read().strip().split('\n')]
         self.monophonic_chars = [line.split('\t') for line in open(monophonic_chars_path).read().strip().split('\n')]
-        self.labels, self.char2phonemes = get_char_phoneme_labels(self.polyphonic_chars) if self.config.use_char_phoneme else get_phoneme_labels(self.polyphonic_chars)
+        self.labels, self.char2phonemes = get_char_phoneme_labels(
+            self.polyphonic_chars) if self.config.use_char_phoneme else get_phoneme_labels(self.polyphonic_chars)
 
         self.chars = sorted(list(self.char2phonemes.keys()))
         self.pos_tags = TextDataset.POS_TAGS
 
-        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                               'bopomofo_to_pinyin_wo_tune_dict.json'), 'r') as fr:
-            self.bopomofo_convert_dict = json.load(fr)
-        self.style_convert_func = {
-            'bopomofo': lambda x: x,
-            'pinyin': self._convert_bopomofo_to_pinyin,
-        }[style]
-
-        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                               'char_bopomofo_dict.json'), 'r') as fr:
-            self.char_bopomofo_dict = json.load(fr)
-
-        self.enable_non_tradional_chinese = enable_non_tradional_chinese
-        if self.enable_non_tradional_chinese:
-            self.s2t_dict = {}
-            for line in open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                    'bert-base-chinese_s2t_dict.txt'), 'r').read().strip().split('\n'):
-                s_char, t_char = line.split('\t')
-                self.s2t_dict[s_char] = t_char
-
-    def _convert_bopomofo_to_pinyin(self, bopomofo):
-        tone = bopomofo[-1]
-        assert tone in '12345'
-        component = self.bopomofo_convert_dict.get(bopomofo[:-1])
-        if component:
-            return component + tone
-        else:
-            print(f'Warning: "{bopomofo}" cannot convert to pinyin')
-            return None
-
-    def _convert_s2t(self, sentence):
-        return ''.join([self.s2t_dict.get(char, char) for char in sentence])
+        self.model = G2PW.from_pretrained(
+            self.model_source,
+            labels=self.labels,
+            chars=self.chars,
+            pos_tags=self.pos_tags,
+            use_conditional=self.config.use_conditional,
+            param_conditional=self.config.param_conditional,
+            use_focal=self.config.use_focal,
+            param_focal=self.config.param_focal,
+            use_pos=self.config.use_pos,
+            param_pos=self.config.param_pos
+        )
+        checkpoint = os.path.join(model_dir, 'best_accuracy.pth')
+        self.model.load_state_dict(torch.load(checkpoint, map_location=self.device))
+        self.model.to(self.device)
 
     def __call__(self, sentences):
         if isinstance(sentences, str):
             sentences = [sentences]
-
-        if self.enable_non_tradional_chinese:
-            translated_sentences = []
-            for sent in sentences:
-                translated_sent = self._convert_s2t(sent)
-                assert len(translated_sent) == len(sent)
-                translated_sentences.append(translated_sent)
-            sentences = translated_sentences
-
+        print(sentences)
+        import time
+        t0 = time.time()
         texts, query_ids, sent_ids, partial_results = self._prepare_data(sentences)
-        if len(texts) == 0:
-            # sentences no polyphonic words
-            return partial_results
 
         dataset = TextDataset(self.tokenizer, self.labels, self.char2phonemes, self.chars, texts, query_ids,
                               use_mask=self.config.use_mask, use_char_phoneme=self.config.use_char_phoneme,
@@ -147,34 +105,28 @@ class G2PWConverter:
             collate_fn=dataset.create_mini_batch,
             num_workers=self.num_workers
         )
-
-        preds, confidences = predict(self.session_g2pw, dataloader, self.labels, turnoff_tqdm=self.turnoff_tqdm)
-
+        print("gen dataloader:", time.time() - t0)
+        preds, confidences = predict(self.model, dataloader, self.device, self.labels)
+        print("gen preds:", time.time() - t0)
         if self.config.use_char_phoneme:
             preds = [pred.split(' ')[1] for pred in preds]
 
-        results = partial_results
+        results = [partial_results]
         for sent_id, query_id, pred in zip(sent_ids, query_ids, preds):
-            results[sent_id][query_id] = self.style_convert_func(pred)
-
+            pred = pred.replace("u:", "v")
+            results[sent_id][query_id] = pred
+        results.append(query_ids)
         return results
 
     def _prepare_data(self, sentences):
+        print(sentences)
         polyphonic_chars = set(self.chars)
-        monophonic_chars_dict = {
-            char: phoneme for char, phoneme in self.monophonic_chars
-        }
-        texts, query_ids, sent_ids, partial_results = [], [], [], []
+        texts, query_ids, sent_ids = [], [], []
         for sent_id, sent in enumerate(sentences):
-            partial_result = [None] * len(sent)
+            partial_results = [item[0] for item in pinyin(sent, neutral_tone_with_five=True, style=Style.TONE3)]
             for i, char in enumerate(sent):
                 if char in polyphonic_chars:
                     texts.append(sent)
                     query_ids.append(i)
                     sent_ids.append(sent_id)
-                elif char in monophonic_chars_dict:
-                    partial_result[i] =  self.style_convert_func(monophonic_chars_dict[char])
-                elif char in self.char_bopomofo_dict:
-                    partial_result[i] =  self.style_convert_func(self.char_bopomofo_dict[char][0])
-            partial_results.append(partial_result)
         return texts, query_ids, sent_ids, partial_results
